525a99c2f1ba1ba6c20f374204f828542bd325fe
Christopher Lamm <chris@theantichris.com>
2019-11-22 10:01:20 -0500

    reading filename from command line

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 // go-quiz is a simple quiz game written in Go.
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a CSV file in the form of 'question,answer'")
+	flag.Parse()
+
+	fmt.Println(*csvFilename)
 }

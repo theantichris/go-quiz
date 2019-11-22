c63fbcc8ca5f7020ce4f1cd04976b7a075ef5233
Christopher Lamm <chris@theantichris.com>
2019-11-22 10:14:26 -0500

    reading CSV file

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a CSV file in the form of 'question,answer'")
 	flag.Parse()
 
-	fmt.Println(*csvFilename)
+	data, err := ioutil.ReadFile(*csvFilename)
+	if err != nil {
+		fmt.Printf("Could not read file %q: %v\n", *csvFilename, err)
+		os.Exit(1)
+	}
+
+	fmt.Print(string(data))
 }
